ab6286767d4aebcf38c13925aeed462ff19c5f9e
lunar-mycroft <lunar-mycroft@users.noreply.github.com>
2018-03-21 15:33:36 -0400

    Fixed speed calculations

--- control/rocketClassDef.cpp
+++ control/rocketClassDef.cpp
@@ -35,24 +35,13 @@ int rocket::createRefrence(Adafruit_BNO055 &bno, Adafruit_BMP280 &baro){
 }
 
 float rocket::getSpeed(){
-    return sqrt(getSpeedSq);
+    v=v+(a*deltaT);
+
+    return v.dot(Q.rotateVector(pointing));
 }
 float rocket::getSpeedSq(){
-	if(!speedUp2Date){
-        float inverseR[9];
-        Matrix.Copy((float *)R,3,3,(float *)inverseR);
-        Matrix.Invert((float *)inverseR,3);
-        Matrix.Multiply((float *)inverseR,(float *)aNRocketFrame,3,3,1,(float * )a);
-        
-        v[0]+=(1000000.0/((float)deltaT))*a[0];
-        v[1]+=(1000000.0/((float)deltaT))*a[1];
-        v[2]+=(1000000.0/((float)deltaT))*a[2];   
-    }
-    updateRotMatrix()
-    float rocketUp[3]={0};
-    Matrix.Multiply((float *)R,(float *)up,3,3,1,(float*)rocketUp)
-
-    return dotProd((float *)v,(float *) rocketUp);
+    float vMag=getSpeed();
+    return vMag*vMag;
 }
 
 int rocket::updateSensorData(Adafruit_BNO055 &bno, Adafruit_BMP280 &baro){
@@ -61,8 +50,8 @@ int rocket::updateSensorData(Adafruit_BNO055 &bno, Adafruit_BMP280 &baro){
         deltaT=float(current-lastUpdate)/1000000.0;
         lastUpdate=current;
 
-        Q = bno.getQuat();
-        a = Q.rotateVector(bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL));
+        Q = bno.getQuat(); //Takes a vector and rotates it by the same amount the BNO has since startup
+        a = Q.rotateVector(bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL)); 
         
         T=baro.readTemperature();
         P=baro.readPressure()
@@ -79,7 +68,7 @@ int rocket::updateSensorData(Adafruit_BNO055 &bno, Adafruit_BMP280 &baro){
 
 float rocket::getPitch(){
     if (!pitchUp2Date){
-        pitch=asin((Q.rotateVector(pointing)).dot(up));
+        pitch=asin(up.dot(Q.rotateVector(pointing)));
     }
     pitchUp2Date = true;
     return pitch;
@@ -92,31 +81,41 @@ float rocket::getRoll(){
         getPitch()
 
 
-        if(pitch>PI/2.0-.01){ // Breaks for the rocket pointed stragith down.  Hopefully won't be an issue.
-
-            Vector<3> axis=pointing.cross(Q.rotateVector(pointing));
+        if(pitch>PI/2.0-0.01 /*Rocket pointing straight up*/){
+            ref=Q.rotateVector(rollRef);
+        } else if(pitch<0.01-PI/2 /*Rocket pointing straight down*/) { 
+            ref=(Q.rotateVector(rollRef))*-1;
+        } else if(pitch>0 /*Rocket pointing above the horizon*/){
+            Vector<3> axis=up.cross(Q.rotateVector(pointing));
             float angle=asin(axis.magnitude());
             axis.normalize();
+
             imu::Quaternion toVertical;
             toVertical.fromAngleAxis(axis,angle);
             ref=toVertical.rotateVector(Q.rotateVector(rollRef));
+        } else { //Rocket pointing bellow the horizon
+            Vector<3> axis=up.cross(Q.rotateVector(pointing));
+            float angle=asin(axis.magnitude());
+            axis.normalize();
 
-        } else {
-            ref=toVertical.rotateVector(rollRef);
+            imu::Quaternion toVertical;
+            toVertical.fromAngleAxis(axis,angle);
+
+            ref=(toVertical.rotateVector(Q.rotateVector(rollRef)))*-1;
         }
 
        if(east.dot(ref)>0){
-           roll=north.dot(ref);
+           roll=acos(north.dot(ref));
        } else {
-           roll=2*PI+north.dot(ref);
+           roll=acos(2*PI+north.dot(ref));
        }
 
         //Calculate roll rate:
         if(oldRoll > 7.0/4.0*PI && roll < 1.0/4.0*PI){ //Roll has likely passed from near all the way around the way around the circle through zero.
-            rollRate=1000000.0*(roll-oldRoll+2.0*PI)/float(deltaT);
+            rollRate=(roll-oldRoll+2.0*PI)/deltaT;
         } else if(roll > 7.0/4.0*PI && oldRoll < 1.0/4.0*PI){ //Roll has likely passed from barely around the circle through zero
-            rollRate=1000000.0*(roll-oldRoll)/float(deltaT);
-        } else rollRate=1000000.0*(roll-oldRoll-2.0*PI)/float(deltaT); //Roll has not passed through zero.
+            rollRate=(roll-oldRoll-2.0*PI)/deltaT;
+        } else rollRate=(roll-oldRoll)/deltaT; //Roll has not passed through zero.
     }
     rollUp2Date = true;
     return roll;
